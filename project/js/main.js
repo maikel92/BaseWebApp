02b796de9ad40a16f23030cb3c443ff82e1b19f7
Maikel Markx <maikelmarkx@gmail.com>
2019-08-26 20:12:58 +0200

    Second commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,26 @@
+function handleSignIn(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+            console.log(user.email());
+  // ...
+            
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+
+
 
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
